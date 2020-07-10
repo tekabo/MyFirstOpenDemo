b94cee64c73db4131d3e8f58712cb6f6b8565292
shangmengmeng <793514513@qq.com>
2020-07-10 16:58:28 +0800

    增加基础知识okhttp用法

--- app/src/main/java/com/ysj/myfirstopendemo/MainActivity.java
+++ app/src/main/java/com/ysj/myfirstopendemo/MainActivity.java
@@ -1,12 +1,5 @@
 package com.ysj.myfirstopendemo;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.LifecycleObserver;
-import androidx.lifecycle.OnLifecycleEvent;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -17,11 +10,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
+
 import com.ysj.dialoglibrary.ProtocolDialog;
 import com.ysj.dialoglibrary.WebUrlActivity;
-
+import com.ysj.listrefreshlibrary.MyRefreshHeader;
 import com.ysj.listrefreshlibrary.RefreshActivity;
 import com.ysj.mergeadapterlibrary.TestMergeActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * 在onCreate方法中添加MyObserver，那么MyObserver就可以观察到MainActivity的各个生命周期的变化
@@ -29,28 +32,31 @@ import com.ysj.mergeadapterlibrary.TestMergeActivity;
  * 默认实现了LifecycleOwner接口,MainActivity是被观察者
  */
 public class MainActivity extends AppCompatActivity {
-    private Button testDialog;
-    private Button testMerge;
-    private Button testLifeCycle;
-    private Button testCall;
-    private Button refreshList;
+    @BindView(R.id.test_dialog_library)
+    Button testDialogLibrary;
+    @BindView(R.id.test_merge_library)
+    Button testMergeLibrary;
+    @BindView(R.id.test_lifecycle)
+    Button testLifecycle;
+    @BindView(R.id.test_call)
+    Button testCall;
+    @BindView(R.id.refresh_list)
+    Button refreshList;
 
     private static final String TAG = "MainActivity";
+    @BindView(R.id.test_okhttp)
+    Button testOkhttp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 //        当Lifecycle的生命周期发生变化时，MyObserver就会观察到，或者说是感知到。MyObserver成为了一个Lifecycle的观察者
         getLifecycle().addObserver(new MyObserver());
 
-        testDialog = findViewById(R.id.test_dialog_library);
-        testMerge = findViewById(R.id.test_merge_library);
-        testLifeCycle = findViewById(R.id.test_lifecycle);
-        testCall = findViewById(R.id.test_call);
-        refreshList = findViewById(R.id.refresh_list);
-
         //测试DialogLibrary
-        testDialog.setOnClickListener(new View.OnClickListener() {
+        testDialogLibrary.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDialog();
@@ -59,19 +65,19 @@ public class MainActivity extends AppCompatActivity {
 
 
         //测试MergeAdapterLibrary
-        testMerge.setOnClickListener(new View.OnClickListener() {
+        testMergeLibrary.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,TestMergeActivity.class);
+                Intent intent = new Intent(MainActivity.this, TestMergeActivity.class);
                 startActivity(intent);
             }
         });
 
         //测试生命周期
-        testLifeCycle.setOnClickListener(new View.OnClickListener() {
+        testLifecycle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            LifeCycleTestActivity.startMyActivity(MainActivity.this);
+                LifeCycleTestActivity.startMyActivity(MainActivity.this);
             }
         });
 
@@ -91,17 +97,25 @@ public class MainActivity extends AppCompatActivity {
                 RefreshActivity.startMyActivity(MainActivity.this);
             }
         });
+
+        //测试okhttp
+        testOkhttp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OkhttpActivity.startMyActivity(MainActivity.this);
+            }
+        });
     }
 
     private void newCall() {
         //检测是否具有权限
-      if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)
-      != PackageManager.PERMISSION_GRANTED){
-          //没有授予权限，去申请
-          ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},1000);
-      }else {
-          callPhone();
-      }
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            //没有授予权限，去申请
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 1000);
+        } else {
+            callPhone();
+        }
 
     }
 
@@ -116,12 +130,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == 1000){
+        if (requestCode == 1000) {
             //检测用户授权结果
-            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 callPhone();
-            }else{
-                Toast.makeText(this,"权限被拒绝",Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "权限被拒绝", Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -133,23 +147,23 @@ public class MainActivity extends AppCompatActivity {
         protocolDialog.setOnBtnClickListener(new ProtocolDialog.BtnClickListener() {
             @Override
             public void theTermsOfServiceClick() {
-                WebUrlActivity.start(MainActivity.this,"https://dova.me/privacy.html","服务条款");
+                WebUrlActivity.start(MainActivity.this, "https://dova.me/privacy.html", "服务条款");
             }
 
             @Override
             public void privacyPolicyClick() {
-                WebUrlActivity.start(MainActivity.this,"https://dova.me/privacy.html","隐私协议");
+                WebUrlActivity.start(MainActivity.this, "https://dova.me/privacy.html", "隐私协议");
             }
 
             @Override
             public void agreeBtnClick() {
-                Toast.makeText(MainActivity.this,"跳转登录页",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "跳转登录页", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void exitBtnClick() {
-               protocolDialog.dismiss();
-               System.exit(0);
+                protocolDialog.dismiss();
+                System.exit(0);
             }
         });
     }
@@ -157,34 +171,36 @@ public class MainActivity extends AppCompatActivity {
 
     public class MyObserver implements LifecycleObserver {
         @OnLifecycleEvent(Lifecycle.Event.ON_START)
-        void onStarts(){
-            Log.e(TAG,"Lifecycle call onStart");
+        void onStarts() {
+            Log.e(TAG, "Lifecycle call onStart");
         }
+
         @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
-        void onResumes(){
-            Log.e(TAG,"Lifecycle call onResume");
+        void onResumes() {
+            Log.e(TAG, "Lifecycle call onResume");
         }
+
         @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
-        void onPauses(){
-            Log.e(TAG,"Lifecycle call onPause");
+        void onPauses() {
+            Log.e(TAG, "Lifecycle call onPause");
         }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.e(TAG,"onStart");
+        Log.e(TAG, "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.e(TAG,"onResume");
+        Log.e(TAG, "onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.e(TAG,"onPause");
+        Log.e(TAG, "onPause");
     }
 }
---
+++ app/src/main/java/com/ysj/myfirstopendemo/OkhttpActivity.java
@@ -0,0 +1,343 @@
+package com.ysj.myfirstopendemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
+/**
+ * 同步调用：编写简单，但是会阻塞主线程，一般不适用
+ * 异步调用：回调函数是在子线程，我们不能在子线程更新UI，需要借助于runOnUiThread()方法或者Handler来处理
+ */
+public class OkhttpActivity extends AppCompatActivity {
+    private static final String URLS = "http://10.0.2.2:8080/androidframelearn/";
+    private static final String URL = "http://wwww.baidu.com";
+    private static final String TAG = "OkhttpActivity";
+
+    public static void startMyActivity(Context context) {
+        Intent intent = new Intent(context, OkhttpActivity.class);
+        context.startActivity(intent);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_okhttp);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick({R.id.get_syn, R.id.get_asy, R.id.post_syn, R.id.post_asy, R.id.btn_post_keyvalue,
+            R.id.btn_post_string, R.id.btn_post_file, R.id.btn_post_form})
+    public void clickBtn(View view) {
+        switch (view.getId()) {
+            case R.id.get_syn:
+                getBySynchronized();
+                break;
+            case R.id.get_asy:
+                getByAsynchronized();
+                break;
+            case R.id.post_syn:
+                postBySynchronized();
+                break;
+            case R.id.post_asy:
+                postByAsynchronized();
+                break;
+            case R.id.btn_post_keyvalue:
+                postKeyValue();
+                break;
+            case R.id.btn_post_string:
+                postString();
+                break;
+            case R.id.btn_post_file:
+                postFile();
+                break;
+            case R.id.btn_post_form:
+                postForm();
+                break;
+        }
+    }
+//    POST请求提交表单
+    private void postForm() {
+        // 1.构建Client对象
+        OkHttpClient client = new OkHttpClient();
+        // 2.构造RequestBody
+        // 2.1 构造文件对象
+        File file = new File(OkhttpActivity.this.getExternalFilesDir(null), "test.png");
+        Log.i(TAG,Environment.getExternalStorageDirectory().toString());
+        // 2.2 判断文件是否为空
+        if (!file.exists()){
+            Log.i(TAG,"文件为空，无法创建");
+        }else{
+            // 2.3 通过表单构造构造RequestBody对象
+            RequestBody muiltipartBody = new MultipartBody.Builder()
+                    //一定要设置这句
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("username", "admin")//
+                    .addFormDataPart("password", "admin")//
+                    .addFormDataPart("myfile", "test.png", RequestBody.create(MediaType.parse("application/octet-stream"), file))
+                    .build();
+            // 3.采用建造者模式和链式调用构建Request对象
+            final Request request = new Request.Builder()
+                    .url(URLS) // 请求URL
+                    .post(muiltipartBody) // 默认就是get请求，可以不写
+                    .build();
+            // 4.通过1和3产生的Client和Request对象生成Call对象
+            Call call = client.newCall(request);
+            // 5.调用Call对象的enqueue()方法，并且实现一个回调实现类
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                    Log.d(TAG, "发送post请求表单失败！");
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    Log.d(TAG, "发送post请求表单成功！请求到的信息为：" + response.body().string());
+                }
+            });
+        }
+    }
+
+    //    POST请求提交文件
+    private void postFile() {
+        // 1.构建Client对象
+        OkHttpClient client = new OkHttpClient();
+        // 2.构造RequestBody
+        // 2.1 构造文件对象
+        File file = new File(OkhttpActivity.this.getExternalFilesDir(null), "test.txt");
+        Log.i(TAG, Environment.getExternalStorageDirectory().toString());//context.getExternalFilesDir()路径为： /storage/emulated/0/Android/data/<包名>/files
+
+
+        // 2.2 判断文件是否为空
+        if(!file.exists()){
+            Log.i(TAG,"文件为空，无法创建");
+        }else {
+            // 2.3 通过文件构造构造RequestBody对象
+            RequestBody requestBody = RequestBody.create(MediaType.parse("application/octet-stream"), file);
+            // 3.采用建造者模式和链式调用构建Request对象
+            final Request request = new Request.Builder()
+                    .url(URLS) // 请求URL
+                    .post(requestBody) // 默认就是get请求，可以不写
+                    .build();
+            // 4.通过1和3产生的Client和Request对象生成Call对象
+            Call call = client.newCall(request);
+            // 5.调用Call对象的enqueue()方法，并且实现一个回调实现类
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                    Log.d(TAG, "发送post请求文件失败！");
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    Log.d(TAG, "发送post请求文件成功！请求到的信息为：" + response.body().string());
+                }
+            });
+        }
+
+    }
+
+    //    POST请求提交字符串
+    private void postString() {
+    // 1.构建Client对象
+        OkHttpClient client = new OkHttpClient();
+        // 2.构造RequestBody
+        RequestBody requestBody = RequestBody.create(MediaType.parse("text/plain;charset=utf-8"), "{username:admin;password:123456}");
+        // 3.采用建造者模式和链式调用构建Request对象
+        final Request request = new Request.Builder()
+                .url(URLS) // 请求URL
+                .post(requestBody) // 默认就是get请求，可以不写
+                .build();
+        // 4.通过1和3产生的Client和Request对象生成Call对象
+        Call call = client.newCall(request);
+        // 5.调用Call对象的enqueue()方法，并且实现一个回调实现类
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.d(TAG, "发送post请求字符串失败！");
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                Log.d(TAG, "发送post请求字符串成功！请求到的信息为：" + response.body().string());
+            }
+        });
+    }
+
+    //    POST请求提交键值对
+    private void postKeyValue() {
+        // 1.构建Client对象
+        OkHttpClient client = new OkHttpClient();
+        // 2.采用建造者模式和链式调用构建键值对对象
+        FormBody formBody = new FormBody.Builder()
+                .add("username", "admin")
+                .add("password", "123456")
+                .build();
+        // 3.采用建造者模式和链式调用构建Request对象
+        final Request request = new Request.Builder()
+                .url(URLS) // 请求URL
+                .post(formBody) // 默认就是get请求，可以不写
+                .build();
+        // 4.通过1和3产生的Client和Request对象生成Call对象
+        Call call = client.newCall(request);
+        // 5.调用Call对象的enqueue()方法，并且实现一个回调实现类
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.d(TAG, "发送post请求键值对失败！");
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                Log.d(TAG, "发送post请求键值对成功！请求到的信息为：" + response.body().string());
+            }
+        });
+    }
+
+
+    //get同步请求
+    private void getBySynchronized() {
+        // 1.构建Client对象
+        OkHttpClient client = new OkHttpClient();
+        // 2.采用建造者模式和链式调用构建Request对象
+        final Request request = new Request.Builder()
+                .url(URL)
+                .get()
+                .build();
+        // 3.通过1和2产生的Client和Request对象生成Call对象
+        final Call call = client.newCall(request);
+        // 4.同步发送get请求需要使用execute()方法，并且为了防止主线程阻塞需要放在子线程中运行
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // 6.构建response对象
+                    Response response = call.execute();
+                    Log.d(TAG, "同步发送get请求成功！请求到的信息为：" + response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    //get异步请求
+    private void getByAsynchronized() {
+        // 1.构建Client对象
+        OkHttpClient client = new OkHttpClient();
+        // 2.采用建造者模式和链式调用构建Request对象
+        final Request request = new Request.Builder()
+                .url(URL) // 请求URL
+                .get() // 默认就是get请求，可以不写
+                .build();
+        // 3.通过1和2产生的Client和Request对象生成Call对象
+        Call call = client.newCall(request);
+        // 4.调用Call对象的enqueue()方法，并且实现一个回调实现类
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.d(TAG, "异步发送get请求失败！");
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                Log.d(TAG, "异步发送get请求成功！请求到的信息为：" + response.body().string());
+            }
+        });
+    }
+
+    //post同步请求
+    private void postBySynchronized() {
+        // 1.构建Client对象
+        OkHttpClient client = new OkHttpClient();
+        // 2.采用建造者模式和链式调用构建键值对对象
+        FormBody formBody = new FormBody.Builder()
+                .add("username", "admin")
+                .add("password", "123456")
+                .build();
+        // 3.采用建造者模式和链式调用构建Request对象
+        final Request request = new Request.Builder()
+                .url(URL)
+                .post(formBody)
+                .build();
+        // 4.通过1和3产生的Client和Request对象生成Call对象
+        final Call call = client.newCall(request);
+        // 5.同步发送post请求需要使用execute()方法，并且为了防止主线程阻塞需要放在子线程中自行
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // 6.构建response对象
+                    Response response = call.execute();
+                    Log.d(TAG, "同步发送post请求成功！请求到的信息为：" + response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    //post异步请求
+    private void postByAsynchronized() {
+        // 1.构建Client对象
+        OkHttpClient client = new OkHttpClient();
+        // 2.采用建造者模式和链式调用构建键值对对象
+        FormBody formBody = new FormBody.Builder()
+                .add("username", "admin")
+                .add("password", "123456")
+                .build();
+        // 3.采用建造者模式和链式调用构建Request对象
+        final Request request = new Request.Builder()
+                .url(URL) // 请求URL
+                .post(formBody) // 默认就是get请求，可以不写
+                .build();
+        // 4.通过1和3产生的Client和Request对象生成Call对象
+        Call call = client.newCall(request);
+        // 5.调用Call对象的enqueue()方法，并且实现一个回调实现类
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Log.d(TAG, "异步发送post请求失败！");
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                Log.d(TAG, "异步发送post请求成功！请求到的信息为：" + response.body().string());
+            }
+        });
+    }
+
+
+}
